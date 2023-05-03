60542b1e1d10675d14735521d81e14af8e8aae33
Alice <120576330+omotashi@users.noreply.github.com>
2023-05-03 18:18:33 +0000

    first commit

---
+++ testing.py
@@ -0,0 +1,8 @@
+userinpute = input("Do you like waffles?")
+
+if userinpute == "Yea we like waffles":
+    userinpute = input("Do you like pancakes?")
+    if userinpute == "Yea we like pancakes":
+        userinpute = input("Do you like french toast?")  
+        if userinpute == "Yea we like french toast":
+            print("doot do do doot doot cant wait to get a mouth full!!")
